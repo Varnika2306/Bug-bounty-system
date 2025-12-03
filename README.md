72b5b68e09e5b201c99f479999ca9d4dc14fc0c5
Varnika2306 <varnika2306@gmail.com>
2025-12-03 12:50:45 +0530

    bip39

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-# scure-bip32
+# scure-bip39
 
-Audited & minimal implementation of BIP32 hierarchical deterministic (HD) wallets over secp256k1.
+Audited & minimal JS implementation of [BIP39 mnemonic phrases](https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki).
 
-- 🔒 [Audited](#security) by an independent security firm
+- 🔒 [**Audited**](#security) by an independent security firm
 - 🔻 Tree-shaking-friendly: use only what's necessary, other code won't be included
 - 📦 ESM and common.js
-- ➰ Only 3 audited dependencies by the same author:
-  [noble-curves](https://github.com/paulmillr/noble-curves),
-  [noble-hashes](https://github.com/paulmillr/noble-hashes),
-  and [scure-base](https://github.com/paulmillr/scure-base)
-- 🪶 300 lines. 90KB with all dependencies
+- ➰ Only 2 audited dependencies by the same author:
+  [noble-hashes](https://github.com/paulmillr/noble-hashes) and [scure-base](https://github.com/paulmillr/scure-base)
+- 🪶 37KB with all deps bundled and 279KB with wordlists: much smaller than similar libraries
 
-Check out [scure-bip39](https://github.com/paulmillr/scure-bip39) if you need mnemonic phrases.
-See [ed25519-keygen](https://github.com/paulmillr/ed25519-keygen) if you need SLIP-0010/BIP32 ed25519 hdkey implementation.
+Check out [scure-bip32](https://github.com/paulmillr/scure-bip32) if you need
+hierarchical deterministic wallets ("HD Wallets").
 
 ### This library belongs to *scure*
 
@@ -30,80 +28,70 @@ See [ed25519-keygen](https://github.com/paulmillr/ed25519-keygen) if you need SL
 
 ## Usage
 
-> npm install @scure/bip32
+> npm install @scure/bip39
 
-This module exports a single class `HDKey`, which should be used like this:
+```js
+import * as bip39 from '@scure/bip39';
+import { wordlist } from '@scure/bip39/wordlists/english';
 
-```ts
-import { HDKey } from "@scure/bip32";
-const hdkey1 = HDKey.fromMasterSeed(seed);
-const hdkey2 = HDKey.fromExtendedKey(base58key);
-const hdkey3 = HDKey.fromJSON({ xpriv: string });
+// Generate x random words. Uses Cryptographically-Secure Random Number Generator.
+const mn = bip39.generateMnemonic(wordlist);
+console.log(mn);
 
-// props
-[hdkey1.depth, hdkey1.index, hdkey1.chainCode];
-console.log(hdkey2.privateKey, hdkey2.publicKey);
-console.log(hdkey3.derive("m/0/2147483647'/1"));
-const sig = hdkey3.sign(hash);
-hdkey3.verify(hash, sig);
+// Reversible: Converts mnemonic string to raw entropy in form of byte array.
+const ent = bip39.mnemonicToEntropy(mn, wordlist)
+
+// Reversible: Converts raw entropy in form of byte array to mnemonic string.
+bip39.entropyToMnemonic(ent, wordlist);
+
+// Validates mnemonic for being 12-24 words contained in `wordlist`.
+bip39.validateMnemonic(mn, wordlist);
+
+// Irreversible: Uses KDF to derive 64 bytes of key data from mnemonic + optional password.
+await bip39.mnemonicToSeed(mn, 'password');
+bip39.mnemonicToSeedSync(mn, 'password');
 ```
 
-Note: `chainCode` property is essentially a private part
-of a secret "master" key, it should be guarded from unauthorized access.
+This submodule contains the word lists defined by BIP39 for Czech, English, French, Italian, Japanese, Korean, Portuguese, Simplified and Traditional Chinese, and Spanish. These are not imported by default, as that would increase bundle sizes too much. Instead, you should import and use them explicitly.
 
-The full API is:
-
-```ts
-class HDKey {
-  public static HARDENED_OFFSET: number;
-  public static fromMasterSeed(seed: Uint8Array, versions: Versions): HDKey;
-  public static fromExtendedKey(base58key: string, versions: Versions): HDKey;
-  public static fromJSON(json: { xpriv: string }): HDKey;
-
-  readonly versions: Versions;
-  readonly depth: number = 0;
-  readonly index: number = 0;
-  readonly chainCode: Uint8Array | null = null;
-  readonly parentFingerprint: number = 0;
-
-  get fingerprint(): number;
-  get identifier(): Uint8Array | undefined;
-  get pubKeyHash(): Uint8Array | undefined;
-  get privateKey(): Uint8Array | null;
-  get publicKey(): Uint8Array | null;
-  get privateExtendedKey(): string;
-  get publicExtendedKey(): string;
-
-  derive(path: string): HDKey;
-  deriveChild(index: number): HDKey;
-  sign(hash: Uint8Array): Uint8Array;
-  verify(hash: Uint8Array, signature: Uint8Array): boolean;
-  wipePrivateData(): this;
-}
-
-interface Versions {
-  private: number;
-  public: number;
-}
+```typescript
+function generateMnemonic(wordlist: string[], strength?: number): string;
+function mnemonicToEntropy(mnemonic: string, wordlist: string[]): Uint8Array;
+function entropyToMnemonic(entropy: Uint8Array, wordlist: string[]): string;
+function validateMnemonic(mnemonic: string, wordlist: string[]): boolean;
+function mnemonicToSeed(mnemonic: string, passphrase?: string): Promise<Uint8Array>;
+function mnemonicToSeedSync(mnemonic: string, passphrase?: string): Uint8Array;
 ```
 
-The module implements [bip32](https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki) standard:
-check it out for additional documentation.
+All wordlists:
 
-The implementation is loosely based on cryptocoinjs/hdkey, [which has MIT License](#LICENSE).
+```typescript
+import { wordlist as czech } from '@scure/bip39/wordlists/czech';
+import { wordlist as english } from '@scure/bip39/wordlists/english';
+import { wordlist as french } from '@scure/bip39/wordlists/french';
+import { wordlist as italian } from '@scure/bip39/wordlists/italian';
+import { wordlist as japanese } from '@scure/bip39/wordlists/japanese';
+import { wordlist as korean } from '@scure/bip39/wordlists/korean';
+import { wordlist as portuguese } from '@scure/bip39/wordlists/portuguese';
+import { wordlist as simplifiedChinese } from '@scure/bip39/wordlists/simplified-chinese';
+import { wordlist as spanish } from '@scure/bip39/wordlists/spanish';
+import { wordlist as traditionalChinese } from '@scure/bip39/wordlists/traditional-chinese';
+```
 
 ## Security
 
+To audit wordlist content, run `node scripts/fetch-wordlist.js`.
+
 The library has been independently audited:
 
-- at version 1.0.1, in Jan 2022, by [cure53](https://cure53.de)
+- at version 1.0.0, in Jan 2022, by [cure53](https://cure53.de)
   - PDFs: [online](https://cure53.de/pentest-report_hashing-libs.pdf), [offline](./audit/2022-01-05-cure53-audit-nbl2.pdf)
-  - [Changes since audit](https://github.com/paulmillr/scure-bip32/compare/1.0.0..main).
+  - [Changes since audit](https://github.com/paulmillr/scure-bip39/compare/1.0.0..main).
   - The audit has been funded by [Ethereum Foundation](https://ethereum.org/en/) with help of [Nomic Labs](https://nomiclabs.io)
 
 The library was initially developed for [js-ethereum-cryptography](https://github.com/ethereum/js-ethereum-cryptography).
 At commit [ae00e6d7](https://github.com/ethereum/js-ethereum-cryptography/commit/ae00e6d7d24fb3c76a1c7fe10039f6ecd120b77e),
-it was extracted to a separate package called `micro-bip32`.
+it was extracted to a separate package called `micro-bip39`.
 After the audit we've decided to use `@scure` NPM namespace for security.
 
 ## License
--- index.js
+++ index.js
@@ -1,269 +1,143 @@
-/*! scure-bip32 - MIT License (c) 2022 Patricio Palladino, Paul Miller (paulmillr.com) */
-import { hmac } from '@noble/hashes/hmac';
-import { ripemd160 } from '@noble/hashes/ripemd160';
-import { sha256 } from '@noble/hashes/sha256';
-import { sha512 } from '@noble/hashes/sha512';
-import { bytes as assertBytes } from '@noble/hashes/_assert';
-import { bytesToHex, concatBytes, createView, hexToBytes, utf8ToBytes } from '@noble/hashes/utils';
-import { secp256k1 as secp } from '@noble/curves/secp256k1';
-import { mod } from '@noble/curves/abstract/modular';
-import { createBase58check } from '@scure/base';
-const Point = secp.ProjectivePoint;
-const base58check = createBase58check(sha256);
-function bytesToNumber(bytes) {
-    return BigInt(`0x${bytesToHex(bytes)}`);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mnemonicToSeedSync = exports.mnemonicToSeed = exports.validateMnemonic = exports.entropyToMnemonic = exports.mnemonicToEntropy = exports.generateMnemonic = void 0;
+/*! scure-bip39 - MIT License (c) 2022 Patricio Palladino, Paul Miller (paulmillr.com) */
+const _assert_1 = require("@noble/hashes/_assert");
+const pbkdf2_1 = require("@noble/hashes/pbkdf2");
+const sha256_1 = require("@noble/hashes/sha256");
+const sha512_1 = require("@noble/hashes/sha512");
+const utils_1 = require("@noble/hashes/utils");
+const base_1 = require("@scure/base");
+// Japanese wordlist
+const isJapanese = (wordlist) => wordlist[0] === '\u3042\u3044\u3053\u304f\u3057\u3093';
+// Normalization replaces equivalent sequences of characters
+// so that any two texts that are equivalent will be reduced
+// to the same sequence of code points, called the normal form of the original text.
+// https://tonsky.me/blog/unicode/#why-is-a----
+function nfkd(str) {
+    if (typeof str !== 'string')
+        throw new TypeError(`Invalid mnemonic type: ${typeof str}`);
+    return str.normalize('NFKD');
 }
-function numberToBytes(num) {
-    return hexToBytes(num.toString(16).padStart(64, '0'));
+function normalize(str) {
+    const norm = nfkd(str);
+    const words = norm.split(' ');
+    if (![12, 15, 18, 21, 24].includes(words.length))
+        throw new Error('Invalid mnemonic');
+    return { nfkd: norm, words };
 }
-const MASTER_SECRET = utf8ToBytes('Bitcoin seed');
-// Bitcoin hardcoded by default
-const BITCOIN_VERSIONS = { private: 0x0488ade4, public: 0x0488b21e };
-export const HARDENED_OFFSET = 0x80000000;
-const hash160 = (data) => ripemd160(sha256(data));
-const fromU32 = (data) => createView(data).getUint32(0, false);
-const toU32 = (n) => {
-    if (!Number.isSafeInteger(n) || n < 0 || n > 2 ** 32 - 1) {
-        throw new Error(`Invalid number=${n}. Should be from 0 to 2 ** 32 - 1`);
-    }
-    const buf = new Uint8Array(4);
-    createView(buf).setUint32(0, n, false);
-    return buf;
+function assertEntropy(entropy) {
+    (0, _assert_1.bytes)(entropy, 16, 20, 24, 28, 32);
+}
+/**
+ * Generate x random words. Uses Cryptographically-Secure Random Number Generator.
+ * @param wordlist imported wordlist for specific language
+ * @param strength mnemonic strength 128-256 bits
+ * @example
+ * generateMnemonic(wordlist, 128)
+ * // 'legal winner thank year wave sausage worth useful legal winner thank yellow'
+ */
+function generateMnemonic(wordlist, strength = 128) {
+    (0, _assert_1.number)(strength);
+    if (strength % 32 !== 0 || strength > 256)
+        throw new TypeError('Invalid entropy');
+    return entropyToMnemonic((0, utils_1.randomBytes)(strength / 8), wordlist);
+}
+exports.generateMnemonic = generateMnemonic;
+const calcChecksum = (entropy) => {
+    // Checksum is ent.length/4 bits long
+    const bitsLeft = 8 - entropy.length / 4;
+    // Zero rightmost "bitsLeft" bits in byte
+    // For example: bitsLeft=4 val=10111101 -> 10110000
+    return new Uint8Array([((0, sha256_1.sha256)(entropy)[0] >> bitsLeft) << bitsLeft]);
 };
-export class HDKey {
-    get fingerprint() {
-        if (!this.pubHash) {
-            throw new Error('No publicKey set!');
-        }
-        return fromU32(this.pubHash);
-    }
-    get identifier() {
-        return this.pubHash;
-    }
-    get pubKeyHash() {
-        return this.pubHash;
-    }
-    get privateKey() {
-        return this.privKeyBytes || null;
-    }
-    get publicKey() {
-        return this.pubKey || null;
-    }
-    get privateExtendedKey() {
-        const priv = this.privateKey;
-        if (!priv) {
-            throw new Error('No private key');
-        }
-        return base58check.encode(this.serialize(this.versions.private, concatBytes(new Uint8Array([0]), priv)));
-    }
-    get publicExtendedKey() {
-        if (!this.pubKey) {
-            throw new Error('No public key');
-        }
-        return base58check.encode(this.serialize(this.versions.public, this.pubKey));
-    }
-    static fromMasterSeed(seed, versions = BITCOIN_VERSIONS) {
-        assertBytes(seed);
-        if (8 * seed.length < 128 || 8 * seed.length > 512) {
-            throw new Error(`HDKey: wrong seed length=${seed.length}. Should be between 128 and 512 bits; 256 bits is advised)`);
-        }
-        const I = hmac(sha512, MASTER_SECRET, seed);
-        return new HDKey({
-            versions,
-            chainCode: I.slice(32),
-            privateKey: I.slice(0, 32),
-        });
-    }
-    static fromExtendedKey(base58key, versions = BITCOIN_VERSIONS) {
-        // => version(4) || depth(1) || fingerprint(4) || index(4) || chain(32) || key(33)
-        const keyBuffer = base58check.decode(base58key);
-        const keyView = createView(keyBuffer);
-        const version = keyView.getUint32(0, false);
-        const opt = {
-            versions,
-            depth: keyBuffer[4],
-            parentFingerprint: keyView.getUint32(5, false),
-            index: keyView.getUint32(9, false),
-            chainCode: keyBuffer.slice(13, 45),
-        };
-        const key = keyBuffer.slice(45);
-        const isPriv = key[0] === 0;
-        if (version !== versions[isPriv ? 'private' : 'public']) {
-            throw new Error('Version mismatch');
-        }
-        if (isPriv) {
-            return new HDKey({ ...opt, privateKey: key.slice(1) });
-        }
-        else {
-            return new HDKey({ ...opt, publicKey: key });
-        }
-    }
-    static fromJSON(json) {
-        return HDKey.fromExtendedKey(json.xpriv);
-    }
-    constructor(opt) {
-        this.depth = 0;
-        this.index = 0;
-        this.chainCode = null;
-        this.parentFingerprint = 0;
-        if (!opt || typeof opt !== 'object') {
-            throw new Error('HDKey.constructor must not be called directly');
-        }
-        this.versions = opt.versions || BITCOIN_VERSIONS;
-        this.depth = opt.depth || 0;
-        this.chainCode = opt.chainCode || null;
-        this.index = opt.index || 0;
-        this.parentFingerprint = opt.parentFingerprint || 0;
-        if (!this.depth) {
-            if (this.parentFingerprint || this.index) {
-                throw new Error('HDKey: zero depth with non-zero index/parent fingerprint');
-            }
-        }
-        if (opt.publicKey && opt.privateKey) {
-            throw new Error('HDKey: publicKey and privateKey at same time.');
-        }
-        if (opt.privateKey) {
-            if (!secp.utils.isValidPrivateKey(opt.privateKey)) {
-                throw new Error('Invalid private key');
-            }
-            this.privKey =
-                typeof opt.privateKey === 'bigint' ? opt.privateKey : bytesToNumber(opt.privateKey);
-            this.privKeyBytes = numberToBytes(this.privKey);
-            this.pubKey = secp.getPublicKey(opt.privateKey, true);
-        }
-        else if (opt.publicKey) {
-            this.pubKey = Point.fromHex(opt.publicKey).toRawBytes(true); // force compressed point
-        }
-        else {
-            throw new Error('HDKey: no public or private key provided');
-        }
-        this.pubHash = hash160(this.pubKey);
-    }
-    derive(path) {
-        if (!/^[mM]'?/.test(path)) {
-            throw new Error('Path must start with "m" or "M"');
-        }
-        if (/^[mM]'?$/.test(path)) {
-            return this;
-        }
-        const parts = path.replace(/^[mM]'?\//, '').split('/');
-        // tslint:disable-next-line
-        let child = this;
-        for (const c of parts) {
-            const m = /^(\d+)('?)$/.exec(c);
-            const m1 = m && m[1];
-            if (!m || m.length !== 3 || typeof m1 !== 'string') {
-                throw new Error(`Invalid child index: ${c}`);
-            }
-            let idx = +m1;
-            if (!Number.isSafeInteger(idx) || idx >= HARDENED_OFFSET) {
-                throw new Error('Invalid index');
-            }
-            // hardened key
-            if (m[2] === "'") {
-                idx += HARDENED_OFFSET;
-            }
-            child = child.deriveChild(idx);
-        }
-        return child;
-    }
-    deriveChild(index) {
-        if (!this.pubKey || !this.chainCode) {
-            throw new Error('No publicKey or chainCode set');
-        }
-        let data = toU32(index);
-        if (index >= HARDENED_OFFSET) {
-            // Hardened
-            const priv = this.privateKey;
-            if (!priv) {
-                throw new Error('Could not derive hardened child key');
-            }
-            // Hardened child: 0x00 || ser256(kpar) || ser32(index)
-            data = concatBytes(new Uint8Array([0]), priv, data);
-        }
-        else {
-            // Normal child: serP(point(kpar)) || ser32(index)
-            data = concatBytes(this.pubKey, data);
-        }
-        const I = hmac(sha512, this.chainCode, data);
-        const childTweak = bytesToNumber(I.slice(0, 32));
-        const chainCode = I.slice(32);
-        if (!secp.utils.isValidPrivateKey(childTweak)) {
-            throw new Error('Tweak bigger than curve order');
-        }
-        const opt = {
-            versions: this.versions,
-            chainCode,
-            depth: this.depth + 1,
-            parentFingerprint: this.fingerprint,
-            index,
-        };
-        try {
-            // Private parent key -> private child key
-            if (this.privateKey) {
-                const added = mod(this.privKey + childTweak, secp.CURVE.n);
-                if (!secp.utils.isValidPrivateKey(added)) {
-                    throw new Error('The tweak was out of range or the resulted private key is invalid');
-                }
-                opt.privateKey = added;
-            }
-            else {
-                const added = Point.fromHex(this.pubKey).add(Point.fromPrivateKey(childTweak));
-                // Cryptographically impossible: hmac-sha512 preimage would need to be found
-                if (added.equals(Point.ZERO)) {
-                    throw new Error('The tweak was equal to negative P, which made the result key invalid');
-                }
-                opt.publicKey = added.toRawBytes(true);
-            }
-            return new HDKey(opt);
-        }
-        catch (err) {
-            return this.deriveChild(index + 1);
-        }
-    }
-    sign(hash) {
-        if (!this.privateKey) {
-            throw new Error('No privateKey set!');
-        }
-        assertBytes(hash, 32);
-        return secp.sign(hash, this.privKey).toCompactRawBytes();
-    }
-    verify(hash, signature) {
-        assertBytes(hash, 32);
-        assertBytes(signature, 64);
-        if (!this.publicKey) {
-            throw new Error('No publicKey set!');
-        }
-        let sig;
-        try {
-            sig = secp.Signature.fromCompact(signature);
-        }
-        catch (error) {
-            return false;
-        }
-        return secp.verify(sig, hash, this.publicKey);
-    }
-    wipePrivateData() {
-        this.privKey = undefined;
-        if (this.privKeyBytes) {
-            this.privKeyBytes.fill(0);
-            this.privKeyBytes = undefined;
-        }
-        return this;
-    }
-    toJSON() {
-        return {
-            xpriv: this.privateExtendedKey,
-            xpub: this.publicExtendedKey,
-        };
-    }
-    serialize(version, key) {
-        if (!this.chainCode) {
-            throw new Error('No chainCode set');
-        }
-        assertBytes(key, 33);
-        // version(4) || depth(1) || fingerprint(4) || index(4) || chain(32) || key(33)
-        return concatBytes(toU32(version), new Uint8Array([this.depth]), toU32(this.parentFingerprint), toU32(this.index), this.chainCode, key);
-    }
+function getCoder(wordlist) {
+    if (!Array.isArray(wordlist) || wordlist.length !== 2048 || typeof wordlist[0] !== 'string')
+        throw new Error('Wordlist: expected array of 2048 strings');
+    wordlist.forEach((i) => {
+        if (typeof i !== 'string')
+            throw new Error(`Wordlist: non-string element: ${i}`);
+    });
+    return base_1.utils.chain(base_1.utils.checksum(1, calcChecksum), base_1.utils.radix2(11, true), base_1.utils.alphabet(wordlist));
 }
-//# sourceMappingURL=index.js.map
+/**
+ * Reversible: Converts mnemonic string to raw entropy in form of byte array.
+ * @param mnemonic 12-24 words
+ * @param wordlist imported wordlist for specific language
+ * @example
+ * const mnem = 'legal winner thank year wave sausage worth useful legal winner thank yellow';
+ * mnemonicToEntropy(mnem, wordlist)
+ * // Produces
+ * new Uint8Array([
+ *   0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f,
+ *   0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f
+ * ])
+ */
+function mnemonicToEntropy(mnemonic, wordlist) {
+    const { words } = normalize(mnemonic);
+    const entropy = getCoder(wordlist).decode(words);
+    assertEntropy(entropy);
+    return entropy;
+}
+exports.mnemonicToEntropy = mnemonicToEntropy;
+/**
+ * Reversible: Converts raw entropy in form of byte array to mnemonic string.
+ * @param entropy byte array
+ * @param wordlist imported wordlist for specific language
+ * @returns 12-24 words
+ * @example
+ * const ent = new Uint8Array([
+ *   0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f,
+ *   0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f
+ * ]);
+ * entropyToMnemonic(ent, wordlist);
+ * // 'legal winner thank year wave sausage worth useful legal winner thank yellow'
+ */
+function entropyToMnemonic(entropy, wordlist) {
+    assertEntropy(entropy);
+    const words = getCoder(wordlist).encode(entropy);
+    return words.join(isJapanese(wordlist) ? '\u3000' : ' ');
+}
+exports.entropyToMnemonic = entropyToMnemonic;
+/**
+ * Validates mnemonic for being 12-24 words contained in `wordlist`.
+ */
+function validateMnemonic(mnemonic, wordlist) {
+    try {
+        mnemonicToEntropy(mnemonic, wordlist);
+    }
+    catch (e) {
+        return false;
+    }
+    return true;
+}
+exports.validateMnemonic = validateMnemonic;
+const salt = (passphrase) => nfkd(`mnemonic${passphrase}`);
+/**
+ * Irreversible: Uses KDF to derive 64 bytes of key data from mnemonic + optional password.
+ * @param mnemonic 12-24 words
+ * @param passphrase string that will additionally protect the key
+ * @returns 64 bytes of key data
+ * @example
+ * const mnem = 'legal winner thank year wave sausage worth useful legal winner thank yellow';
+ * await mnemonicToSeed(mnem, 'password');
+ * // new Uint8Array([...64 bytes])
+ */
+function mnemonicToSeed(mnemonic, passphrase = '') {
+    return (0, pbkdf2_1.pbkdf2Async)(sha512_1.sha512, normalize(mnemonic).nfkd, salt(passphrase), { c: 2048, dkLen: 64 });
+}
+exports.mnemonicToSeed = mnemonicToSeed;
+/**
+ * Irreversible: Uses KDF to derive 64 bytes of key data from mnemonic + optional password.
+ * @param mnemonic 12-24 words
+ * @param passphrase string that will additionally protect the key
+ * @returns 64 bytes of key data
+ * @example
+ * const mnem = 'legal winner thank year wave sausage worth useful legal winner thank yellow';
+ * mnemonicToSeedSync(mnem, 'password');
+ * // new Uint8Array([...64 bytes])
+ */
+function mnemonicToSeedSync(mnemonic, passphrase = '') {
+    return (0, pbkdf2_1.pbkdf2)(sha512_1.sha512, normalize(mnemonic).nfkd, salt(passphrase), { c: 2048, dkLen: 64 });
+}
+exports.mnemonicToSeedSync = mnemonicToSeedSync;
