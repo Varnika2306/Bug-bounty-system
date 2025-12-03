bbf308793762d13b1ad00891bd946b92b1fee8e5
Varnika2306 <varnika2306@gmail.com>
2025-12-03 12:55:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-"use strict";
-throw new Error("This package has no entry-point. Please consult the README.md to learn how to use it.");
+export { generateMnemonic, mnemonicToEntropy, entropyToMnemonic, validateMnemonic, mnemonicToSeed, mnemonicToSeedSync } from "@scure/bip39";
