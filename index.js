a192723568e279d0974038e58a810496b64e8d92
Varnika2306 <varnika2306@gmail.com>
2025-12-03 12:51:11 +0530

    bip39 esm

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mnemonicToSeedSync = exports.mnemonicToSeed = exports.validateMnemonic = exports.entropyToMnemonic = exports.mnemonicToEntropy = exports.generateMnemonic = void 0;
 /*! scure-bip39 - MIT License (c) 2022 Patricio Palladino, Paul Miller (paulmillr.com) */
-const _assert_1 = require("@noble/hashes/_assert");
-const pbkdf2_1 = require("@noble/hashes/pbkdf2");
-const sha256_1 = require("@noble/hashes/sha256");
-const sha512_1 = require("@noble/hashes/sha512");
-const utils_1 = require("@noble/hashes/utils");
-const base_1 = require("@scure/base");
+import { bytes as assertBytes, number as assertNumber } from '@noble/hashes/_assert';
+import { pbkdf2, pbkdf2Async } from '@noble/hashes/pbkdf2';
+import { sha256 } from '@noble/hashes/sha256';
+import { sha512 } from '@noble/hashes/sha512';
+import { randomBytes } from '@noble/hashes/utils';
+import { utils as baseUtils } from '@scure/base';
 // Japanese wordlist
 const isJapanese = (wordlist) => wordlist[0] === '\u3042\u3044\u3053\u304f\u3057\u3093';
 // Normalization replaces equivalent sequences of characters
@@ -27,7 +24,7 @@ function normalize(str) {
     return { nfkd: norm, words };
 }
 function assertEntropy(entropy) {
-    (0, _assert_1.bytes)(entropy, 16, 20, 24, 28, 32);
+    assertBytes(entropy, 16, 20, 24, 28, 32);
 }
 /**
  * Generate x random words. Uses Cryptographically-Secure Random Number Generator.
@@ -37,19 +34,18 @@ function assertEntropy(entropy) {
  * generateMnemonic(wordlist, 128)
  * // 'legal winner thank year wave sausage worth useful legal winner thank yellow'
  */
-function generateMnemonic(wordlist, strength = 128) {
-    (0, _assert_1.number)(strength);
+export function generateMnemonic(wordlist, strength = 128) {
+    assertNumber(strength);
     if (strength % 32 !== 0 || strength > 256)
         throw new TypeError('Invalid entropy');
-    return entropyToMnemonic((0, utils_1.randomBytes)(strength / 8), wordlist);
+    return entropyToMnemonic(randomBytes(strength / 8), wordlist);
 }
-exports.generateMnemonic = generateMnemonic;
 const calcChecksum = (entropy) => {
     // Checksum is ent.length/4 bits long
     const bitsLeft = 8 - entropy.length / 4;
     // Zero rightmost "bitsLeft" bits in byte
     // For example: bitsLeft=4 val=10111101 -> 10110000
-    return new Uint8Array([((0, sha256_1.sha256)(entropy)[0] >> bitsLeft) << bitsLeft]);
+    return new Uint8Array([(sha256(entropy)[0] >> bitsLeft) << bitsLeft]);
 };
 function getCoder(wordlist) {
     if (!Array.isArray(wordlist) || wordlist.length !== 2048 || typeof wordlist[0] !== 'string')
@@ -58,7 +54,7 @@ function getCoder(wordlist) {
         if (typeof i !== 'string')
             throw new Error(`Wordlist: non-string element: ${i}`);
     });
-    return base_1.utils.chain(base_1.utils.checksum(1, calcChecksum), base_1.utils.radix2(11, true), base_1.utils.alphabet(wordlist));
+    return baseUtils.chain(baseUtils.checksum(1, calcChecksum), baseUtils.radix2(11, true), baseUtils.alphabet(wordlist));
 }
 /**
  * Reversible: Converts mnemonic string to raw entropy in form of byte array.
@@ -73,13 +69,12 @@ function getCoder(wordlist) {
  *   0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f
  * ])
  */
-function mnemonicToEntropy(mnemonic, wordlist) {
+export function mnemonicToEntropy(mnemonic, wordlist) {
     const { words } = normalize(mnemonic);
     const entropy = getCoder(wordlist).decode(words);
     assertEntropy(entropy);
     return entropy;
 }
-exports.mnemonicToEntropy = mnemonicToEntropy;
 /**
  * Reversible: Converts raw entropy in form of byte array to mnemonic string.
  * @param entropy byte array
@@ -93,16 +88,15 @@ exports.mnemonicToEntropy = mnemonicToEntropy;
  * entropyToMnemonic(ent, wordlist);
  * // 'legal winner thank year wave sausage worth useful legal winner thank yellow'
  */
-function entropyToMnemonic(entropy, wordlist) {
+export function entropyToMnemonic(entropy, wordlist) {
     assertEntropy(entropy);
     const words = getCoder(wordlist).encode(entropy);
     return words.join(isJapanese(wordlist) ? '\u3000' : ' ');
 }
-exports.entropyToMnemonic = entropyToMnemonic;
 /**
  * Validates mnemonic for being 12-24 words contained in `wordlist`.
  */
-function validateMnemonic(mnemonic, wordlist) {
+export function validateMnemonic(mnemonic, wordlist) {
     try {
         mnemonicToEntropy(mnemonic, wordlist);
     }
@@ -111,7 +105,6 @@ function validateMnemonic(mnemonic, wordlist) {
     }
     return true;
 }
-exports.validateMnemonic = validateMnemonic;
 const salt = (passphrase) => nfkd(`mnemonic${passphrase}`);
 /**
  * Irreversible: Uses KDF to derive 64 bytes of key data from mnemonic + optional password.
@@ -123,10 +116,9 @@ const salt = (passphrase) => nfkd(`mnemonic${passphrase}`);
  * await mnemonicToSeed(mnem, 'password');
  * // new Uint8Array([...64 bytes])
  */
-function mnemonicToSeed(mnemonic, passphrase = '') {
-    return (0, pbkdf2_1.pbkdf2Async)(sha512_1.sha512, normalize(mnemonic).nfkd, salt(passphrase), { c: 2048, dkLen: 64 });
+export function mnemonicToSeed(mnemonic, passphrase = '') {
+    return pbkdf2Async(sha512, normalize(mnemonic).nfkd, salt(passphrase), { c: 2048, dkLen: 64 });
 }
-exports.mnemonicToSeed = mnemonicToSeed;
 /**
  * Irreversible: Uses KDF to derive 64 bytes of key data from mnemonic + optional password.
  * @param mnemonic 12-24 words
@@ -137,7 +129,6 @@ exports.mnemonicToSeed = mnemonicToSeed;
  * mnemonicToSeedSync(mnem, 'password');
  * // new Uint8Array([...64 bytes])
  */
-function mnemonicToSeedSync(mnemonic, passphrase = '') {
-    return (0, pbkdf2_1.pbkdf2)(sha512_1.sha512, normalize(mnemonic).nfkd, salt(passphrase), { c: 2048, dkLen: 64 });
+export function mnemonicToSeedSync(mnemonic, passphrase = '') {
+    return pbkdf2(sha512, normalize(mnemonic).nfkd, salt(passphrase), { c: 2048, dkLen: 64 });
 }
-exports.mnemonicToSeedSync = mnemonicToSeedSync;
